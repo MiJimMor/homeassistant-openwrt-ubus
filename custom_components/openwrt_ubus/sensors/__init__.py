2c79c3ab490ae78d5649d936e822dc7506feb128
MiJimMor <142923677+MiJimMor@users.noreply.github.com>
2026-01-07 06:43:00 +0100

    Register connected_devices_sensor module

--- custom_components/openwrt_ubus/sensors/__init__.py
+++ custom_components/openwrt_ubus/sensors/__init__.py
@@ -1,5 +1,5 @@
 """Sensor modules for OpenWrt ubus integration."""
 
-from . import ap_sensor, eth_sensor, qmodem_sensor, sta_sensor, system_sensor
+from . import ap_sensor, eth_sensor, qmodem_sensor, sta_sensor, system_sensor, connected_devices_sensor
 
-__all__ = ["ap_sensor", "eth_sensor", "qmodem_sensor", "sta_sensor", "system_sensor"]
+__all__ = ["ap_sensor", "eth_sensor", "qmodem_sensor", "sta_sensor", "system_sensor", "connected_devices_sensor"]
